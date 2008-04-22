44b257f931d8bb88d449ca91b59e2829bf5c3e1c
Court3nay <courtenay@caboo.se>
2008-04-22 16:31:22 -0700

    current_member, cleanup

--- vendor/plugins/tentacled_beast/init.rb
+++ vendor/plugins/tentacled_beast/init.rb
@@ -2,40 +2,11 @@
 
 Rails.plugin_routes << :tentacled_beast
 
-## these don't load from initializers, yet!?!?!?
-## WTF!
-class ActiveRecord::Base
-  @@white_list_sanitizer = HTML::WhiteListSanitizer.new
-  class << self
-    attr_accessor :formatted_attributes
-  end
-
-  cattr_reader :white_list_sanitizer
-
-  def self.formats_attributes(*attributes)
-    (self.formatted_attributes ||= []).push *attributes
-    before_save :format_attributes
-    send :include, HtmlFormatting, ActionView::Helpers::TagHelper, ActionView::Helpers::TextHelper
-  end
-
-  def self.paginated_each(options = {}, &block)
-    page = 1
-    records = [nil]
-    until records.empty? do 
-      records = paginate(options.update(:page => page, :count => {:select => '*'}))
-      records.each &block
-      page += 1
-    end
-  end
+$stderr.puts "init.rb"
+Dir["#{File.dirname(__FILE__)}/lib/initializers/*.rb"].each do |f|
+  $stderr.puts "Requiring #{f}"
+  require f
 end
-
-class << ActiveRecord::Base
-  def concerned_with(*concerns)
-    concerns.each { |c| require_dependency "#{name.underscore}/#{c}" }
-  end
-end
-########
-
 
 # Some models have to be forcefully required
 # because of namespace type clashes
@@ -47,18 +18,31 @@ module Tentacle::Application::ForumMethods
   def self.included(base)
     $stderr.puts "Set forum sidebar"
     base.before_filter :set_forums_sidebar
+    base.append_before_filter :set_group_forum_link
     base.helper_method :current_group
+    base.helper_method :current_member
     base.helper_method :current_page
   end
   
   def set_forums_sidebar
-    $stderr.puts "Set forum sidebar"
     @content_for_sidebar ||= ""
     @content_for_sidebar += "<p>Forums plugin installed. <a href='/forums'>Go to forums</a></p>"
   end
+  
+  def set_group_forum_link
+    @content_for_group_show ||= ""
+    if @group or current_group
+      @content_for_group_show << render_to_string(:partial => "groups/forums") unless RAILS_ENV=='test'
+    end
+  end
+
+  def current_member
+    return Membership.join_from(current_group.id, current_profile.id)
+  end
 
   def current_group
-    @current_group ||= Group.find_by_name("Default") || Group.find(:first)
+    logger.warn "CURRENT GROUP!!"
+    @group ||= Group.find_by_name((params[:group_id] || "Default").titleize) || Group.find(:first)
   end
 
   def current_page
